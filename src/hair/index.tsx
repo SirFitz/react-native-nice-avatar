6b28ddd0e6e434d62559e1db57dadb7041aef7e3
sirfitz <sirromariof@gmail.com>
2022-02-14 00:09:10 -0500

    added locks

--- src/hair/index.tsx
+++ src/hair/index.tsx
@@ -6,12 +6,14 @@ import Thick from "./thick";
 import Mohawk from "./mohawk";
 import WomanLong from "./womanLong";
 import WomanShort from "./womanShort";
+import Locks from "./locks";
 
 export default function hair(props: { style: string, color: string, colorRandom: boolean }): any {
   const { style, color, colorRandom } = props;
   switch (style) {
     case "thick": return <Thick color={color} colorRandom={colorRandom} />;
     case "mohawk": return <Mohawk color={color} colorRandom={colorRandom} />;
+    case "locks": return <Locks color={color} />;
     case "womanLong": return <WomanLong color={color} />;
     case "womanShort": return <WomanShort color={color} />;
     case "normal":
